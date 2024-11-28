5ca46f982f7380db69068ed4af72380afa74b8e1
HHUUYYLLEE <lebahuybk@gmail.com>
2024-11-28 16:08:32 +0700

    shazam search

--- src/app/(api)/shazamSearch.ts
+++ src/app/(api)/shazamSearch.ts
@@ -5,7 +5,7 @@ const shazamAPI = axios.create({
   baseURL: envConfig.shazamHost,
   headers: {
     "x-rapidapi-ua": "RapidAPI-Playground",
-    "x-rapidapi-key": "5af8e97c90mshf86894f23665fbbp194430jsn3c15333144b2",
+    "x-rapidapi-key": envConfig.shazamAPI,
     "x-rapidapi-host": envConfig.shazamHost?.replace("https://", ""),
     specificMethodHeaders: "[object Object]",
   },
